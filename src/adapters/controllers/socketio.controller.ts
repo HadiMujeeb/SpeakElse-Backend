c4f018d119574ef72542451bba3a1341a1c47edf
Hadi <hadimujeeb300@gmail.com>
2025-01-10 09:17:24 +0530

    update

--- src/adapters/controllers/socketio.controller.ts
+++ src/adapters/controllers/socketio.controller.ts
@@ -12,7 +12,7 @@ export class SocketioController {
     this.prisma = new PrismaClient(); // Initialize PrismaClient
     this.io = new Server(httpServer, {
       cors: {
-        origin: process.env.FRONTEND_URL,
+        origin: 'http://localhost:4200',
         credentials: true,
       },
     });
